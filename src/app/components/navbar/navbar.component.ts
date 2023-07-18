aad80583356839998482e7028663e01a5d410243
Ankush Choudhary <ankushchoudhary@jklu.edu.in>
2023-07-18 22:30:57 +0530

    feature section

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -35,22 +35,28 @@ export class NavbarComponent {
   scrollToSection(sectionId: string) {
     const sectionElement = document.getElementById(sectionId);
     if (sectionElement) {
-      sectionElement.scrollIntoView({ behavior: 'smooth' });
+      // Calculate the offset to center the section on the screen
+      const windowHeight = window.innerHeight;
+      const sectionHeight = sectionElement.clientHeight;
+      const offset = Math.max((windowHeight - sectionHeight) / 2, 0);
+  
+      // Scroll to the section with the calculated offset
+      sectionElement.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
     } else {
       console.warn(`Element with ID '${sectionId}' not found.`);
     }
   }
   updateActiveSection() {
     const sections = document.querySelectorAll('section');
-    const scrollPosition = window.pageYOffset + this.offset; // Add offset here
-
+    const scrollPosition = window.pageYOffset + this.offset;
+  
     // Find the currently visible section based on the section IDs
     for (let i = 0; i < sections.length; i++) {
       const section = sections[i];
-      const sectionTop = section.offsetTop;
+      const sectionTop = section.offsetTop - this.offset; // Update the sectionTop calculation
       const sectionBottom = sectionTop + section.offsetHeight;
       const sectionId = section.getAttribute('id');
-
+  
       if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
         if (sectionId !== this.activeSection) {
           this.activeSection = sectionId;
